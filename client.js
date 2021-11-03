80dab659046dfe247254ae0296549b3d27e65ed4
Muaad Ahmed <muaad.abdi.ahmed@gmail.com>
2021-11-03 22:52:01 +0000

    send a name upon connecting to the server

---
+++ client.js
@@ -0,0 +1,24 @@
+const net = require('net');
+const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: 50541 // PORT number here,
+  });
+
+  const name = 'MA';
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on('data', (data) => {
+    console.log(data);
+  });
+
+  conn.on('connect', () => {
+    console.log('Successfully connected to snake server...');
+    conn.write(`Name: ${name}`);
+  });
+
+  return conn;
+};
+
+module.exports = {connect};
