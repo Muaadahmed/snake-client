4b7102a0bb662798cc83bb7fb5e9c339edc0d495
Muaad Ahmed <muaad.abdi.ahmed@gmail.com>
2021-11-04 00:10:59 +0000

    added exit key to exit server

--- play.js
+++ play.js
@@ -1,4 +1,21 @@
 const net = require("net");
 const {connect} = require('./client');
 connect();
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  const handleUserInput = function(key) {
+    if (key === '\u0003') {
+      process.exit();
+    } else if (key === 'w') {
+      console.log(key);
+    }
+  };
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+setupInput();
 // establishes a connection with the game server
